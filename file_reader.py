9f2f3b778b4cd1f58cb33657fb44d2600a41ea68
NathanSan777 <0nananate0@gmail.com>
2023-12-08 19:02:36 -0500

    The beginning to our project! Let's go, team!

---
+++ file_reader.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import sklearn as sk
+import nltk
+import random
+import matplotlib.pyplot as plt
+
+# A method to quickly convert our csv data into a dataframe.
+# A value of 0 indivates that it is a human-written review,
+# and a value of 1 indicates that it is a machine-generated review.
+def convert_csv_to_dataframe(filepath):
+    df = pd.read_csv(filepath)
+    df = df.dropna()
+    # print(df)
+    # print("File has been read and converted to a dataframe!")
+    return df
+
+
+def get_random_lines(df, amount):
+    # print("Getting a random selection of ", amount, "rows in this dataframe.")
+    total_rows = df.shape[0]
+    random_indices = random.sample(range(total_rows), amount)
+    random_selection_df = df.iloc[random_indices]
+    return random_selection_df
+
+
+def main():
+    print("Hello! Beginning to read file...")
+    file = './review_data/Training_Essay_Data.csv'
+    df = convert_csv_to_dataframe(file)
+    sample_df = get_random_lines(df, 100)
+    print(sample_df)
+    
+
+main()
